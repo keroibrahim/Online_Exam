aa95ef8f4fc567c290801dac03b4fe9e8c070929
Kerollos Ibrahim <kerollos29@gmail.com>
2024-03-05 17:47:29 +0200

    Update admin-routes.js

--- src/features/Admin/routes/admin-routes.js
+++ src/features/Admin/routes/admin-routes.js
@@ -1,2 +1,22 @@
+// still modifying on it 
 
-still modifying on it 
+const express = require('express');
+const router = express.Router();
+const adminController = require('../Admin/controller');
+
+// Create a new admin
+router.post('/', adminController.createAdmin);
+
+// Retrieve all admins
+router.get('/', adminController.getAllAdmins);
+
+// Retrieve an admin by ID
+router.get('/:id', adminController.getAdminById);
+
+// Update an admin
+router.put('/:id', adminController.updateAdmin);
+
+// Delete an admin
+router.delete('/:id', adminController.deleteAdmin);
+
+module.exports = router;
