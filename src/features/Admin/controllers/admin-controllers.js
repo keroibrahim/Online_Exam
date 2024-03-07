e1002b2a337dc7c9270b3cff778538657ed1590f
Kerollos Ibrahim <kerollos29@gmail.com>
2024-03-07 19:33:26 +0200

    Update admin-controllers.js

--- src/features/Admin/controllers/admin-controllers.js
+++ src/features/Admin/controllers/admin-controllers.js
@@ -1,10 +1,20 @@
-
-import Admin from '../Admin/models'; // admin model file
+import Admin from '../models/Admin'; // admin model file
+import { isValidPhoneNumber } from 'libphonenumber-js';
+import bcrypt from 'bcryptjs';
 
 // Fn create a new admin
 exports.createAdmin = async (req, res) => {
   try {
-    const newAdmin = new Admin(req.body);
+    // Validate phone no.
+    if (req.body.phone && !isValidPhoneNumber(req.body.phone, 'EG')) {
+      return res.status(400).json({ success: false, message: `${req.body.phone} is not a valid phone number!` });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // Create a new admin with the hashed password
+    const newAdmin = new Admin({ name: req.body.name, password: hashedPassword, email: req.body.email, gender: req.body.gender, phone: req.body.phone });
     await newAdmin.save();
     res.status(201).json({ success: true, data: newAdmin });
   } catch (error) {
@@ -38,6 +48,12 @@ exports.getAdminById = async (req, res) => {
 // Fn update an admin
 exports.updateAdmin = async (req, res) => {
   try {
+    // Hash the password if it is provided in the request body
+    if (req.body.password) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      req.body.password = hashedPassword;
+    }
+
     const updatedAdmin = await Admin.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!updatedAdmin) {
       return res.status(404).json({ success: false, message: 'Admin not found' });
